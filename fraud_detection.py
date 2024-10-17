9262dcae18740a880056de6d37d7d195d685fa75
Calastrophe <zediant@gmail.com>
2024-10-17 13:17:41 -0500

    pytorch conversion, better batching

---
+++ fraud_detection.py
@@ -0,0 +1,78 @@
+import polars
+import torch
+import pytorch_lightning as pl
+from torch import nn
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, TensorDataset
+
+
+class NeuralNet(pl.LightningModule):
+    def __init__(self):
+        super(NeuralNet, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(29, 16),
+            nn.ReLU(),
+            nn.Linear(16, 24),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(24, 24),
+            nn.ReLU(),
+            nn.Linear(24, 1),
+            nn.Sigmoid(),
+        )
+        self.loss_fn = nn.BCELoss()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
+
+    def training_step(self, batch):
+        x, y = batch
+        y_hat = self.forward(x).squeeze()
+        loss = self.loss_fn(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def test_step(self, batch):
+        X, y = batch
+        y_hat = self(X)
+        y_pred = (y_hat > 0.5).float()
+        accuracy = (y_pred.squeeze() == y).float().mean()
+        self.log("test_accuracy", accuracy)
+        return accuracy
+
+
+if __name__ == "__main__":
+    torch.set_float32_matmul_precision('medium')
+    torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    df = polars.read_csv("creditcard.csv", schema_overrides={
+                         "Time": polars.Utf8}).drop("Time")
+
+    X = df.select(polars.all().exclude("Class")).to_numpy()
+    y = df.select("Class").to_numpy().flatten()
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2
+    )
+
+    train_dataset = TensorDataset(
+        torch.FloatTensor(X_train), torch.FloatTensor(y_train)
+    )
+    test_dataset = TensorDataset(
+        torch.FloatTensor(X_test), torch.FloatTensor(y_test))
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=64, shuffle=True, num_workers=16)
+
+    test_loader = DataLoader(test_dataset, batch_size=64, num_workers=16)
+
+    model = NeuralNet()
+
+    trainer = pl.Trainer(max_epochs=5)
+
+    trainer.fit(model, train_loader)
+
+    trainer.test(model, test_loader)
